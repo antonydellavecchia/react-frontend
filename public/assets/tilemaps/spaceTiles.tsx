d352f497f055d1308a0011561313ab94b20898bf
Antony <antonydellavecchia@gmail.com>
2019-01-14 15:05:06 +0000

    add assets update

---
+++ public/assets/tilemaps/spaceTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Space Kitchen" tilewidth="50" tileheight="50" tilecount="374" columns="22">
+ <image source="../images/space_tiles.png" width="1114" height="898"/>
+</tileset>
